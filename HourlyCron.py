084f468132650273ea3db08c375367e4d4697aa6
larmalade <lmadeo76@gmail.com>
2016-06-27 22:47:11 -0500

    false needs to be capitolized :/

--- HourlyCron.py
+++ HourlyCron.py
@@ -509,7 +509,7 @@ day2shortDay = { 'Monday' : 'Mon', 'Tuesday' : 'Tue',
 day2num = {'Monday':0, 'Tuesday':1, 'Wednesday':2, 'Thursday':3,
            'Friday':4, 'Saturday':5, 'Sunday':6}
            
-DEBUGGING = false
+DEBUGGING = False
    
 
 if __name__ == '__main__':
